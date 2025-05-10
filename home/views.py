726caaf780cd299a0c85ab9ea0d50d75368e2f24
ShmidtAna <99461376+ShmidtAna@users.noreply.github.com>
2025-05-10 14:36:14 +0700

    Update views.py

--- home/views.py
+++ home/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def credits(request):
-    content = "Nicky\nOlesya"
+    content = "Nicky\nNastya"
     return HttpResponse(content, content_type="text/plain")
 
 def news(request):
